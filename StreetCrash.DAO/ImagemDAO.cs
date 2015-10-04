a17c18e373c0ed28958b64692799cddb00705c2e
Érico Ribeiro Nobre <ericor25@gmail.com>
2015-10-03 22:16:00 -0300

    Correção do commit

---
+++ StreetCrash.DAO/ImagemDAO.cs
@@ -0,0 +1,30 @@
+﻿using StreetCrash.DAO.LinqToSql;
+using StreetCrash.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetCrash.DAO
+{
+    public class ImagemDAO
+    {
+        StreetCrashDataContext _context;
+        public ImagemDAO()
+        {
+            _context = new StreetCrashDataContext();
+        }
+
+        public void Inserir(Imagem model)
+        { 
+            _context.IMAGEMs.InsertOnSubmit(new IMAGEM
+            {
+                CODE_OCORRENCIA = model.Code_Ocorrencia,
+                IMAGEM1 = new System.Data.Linq.Binary(model.Imagem_Ocorrencia.ToArray()),
+                DESCRICAO = model.Descricao
+            });
+            _context.SubmitChanges();
+        }
+    }
+}
---
+++ StreetCrash.DAO/OcorrenciaDAO.cs
@@ -0,0 +1,43 @@
+﻿using StreetCrash.DAO.LinqToSql;
+using StreetCrash.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetCrash.DAO
+{
+    public class OcorrenciaDAO
+    {
+        StreetCrashDataContext _context;
+        public OcorrenciaDAO()
+        {
+            _context = new StreetCrashDataContext();
+        }
+        public void Inserir(Ocorrencia model)
+        {
+            _context.OCORRENCIAs.InsertOnSubmit(new OCORRENCIA
+            {
+                CODE_CATEGORIA = model.Code_Categoria,
+                CODE_STATUS = model.Code_Status,
+                LATITUDE = model.Latitude,
+                LONGITUDE = model.Longitude,
+                ENDERECO = model.Endereco,
+                DATA_CADASTRO = model.Data_Cadastro,
+                OUTRO = model.Outro,
+                RESOLVIDO = false
+            });
+            _context.SubmitChanges();
+        }
+
+        public void UpdateStatus(UpdateStatus model)
+        {
+            OCORRENCIA ocorrencia = _context.OCORRENCIAs.Where(o => o.CODE == model.Code).SingleOrDefault();
+            ocorrencia.RESOLVIDO = model.Resolvido;
+            _context.SubmitChanges();
+        } 
+        
+               
+    }
+}
